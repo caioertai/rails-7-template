0d00e157be9d2d6cabdfdede35bfadf6c119d970
Caio Andrade <andrade@hey.com>
2022-07-04 00:33:12 -0500

    Add DART sass config

--- template.rb
+++ template.rb
@@ -1,28 +1,29 @@
 gem "haml-rails"
+gem "dartsass-rails"
 
 gem_group :development do
-  gem "cssbundling-rails"
   gem "erb2haml"
   gem "standardrb"
 end
 
 after_bundle do
-  rails_command  "haml:replace_erbs"
-  rails_command  "css:install:sass"
+  # Create and Migrate DB
+  rails_command  "db:create db:migrate"
 
+  # Replace ERB templates with HAML
+  rails_command  "haml:replace_erbs"
+
+  # Add home page
   generate(:controller, :pages, :home, "--skip-routes", "--no-test-framework", "--skip-helper")
   route 'root "pages#home"'
 
-  # The following is the answer to:
-  # Add "scripts": { "build:css": "sass ./app/assets/stylesheets/application.sass.scss:./app/assets/builds/application.css --no-source-map --load-path=node_modules" } to your package.json
-  inject_into_file "package.json", after: "  }" do
-    <<~TXT
-      ,
-        "scripts": {
-          "build:css": "sass ./app/assets/stylesheets/application.sass.scss:./app/assets/builds/application.css --no-source-map --load-path=node_modules"
-        }
-    TXT
-  end
-
-  rails_command  "db:create db:migrate"
+  # DART SASS Config
+  rails_command  "dartsass:install"
+  run "rm app/assets/stylesheets/application.css"
+  run "mv app/assets/stylesheets/application.scss app/assets/stylesheets/application.sass"
+  initializer "dart_sass.rb", <<~RUBY
+    Rails.application.config.dartsass.builds = {
+      "application.sass"  => "application.css",
+    }
+  RUBY
 end
